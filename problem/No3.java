e6cdbbef382f07059f5690884c1d22beab88cdeb
Yahiko-r <254618796@qq.com>
2019-01-02 16:58:23 +0800

    数组中重复数字

--- problem/No3.java
+++ problem/No3.java
@@ -1,4 +1,19 @@
 package problem;
-
+//一个数组中找有没有重复数字，范围从0~length-1
 public class No3 {
+    private boolean isDuplicate(int[] nums){
+        int length=nums.length;
+        if(length==0)return false;
+        for(int i=0;i<length;i++){
+            if(nums[i]<0||nums[i]>length-1)return false;
+            while(nums[i]!=i){
+                if(nums[i]==nums[nums[i]])return true;
+                int temp=nums[i];
+                nums[i]=nums[temp];
+                nums[temp]=temp;
+            }
+
+        }
+        return false;
+    }
 }
